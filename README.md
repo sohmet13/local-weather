386e1dab7013947d4e4f0bbeefffabcf9c475964
Лощакова Анна Александровна <17537870@sigma.sbrf.ru>
2019-08-13 15:35:18 +0300

    refactor logic

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Local-Weather
 The project shows local weather. Made with the usage of JQuery
 
-The project is available on CodePen.
-https://codepen.io/sohmet13/full/mXdJwE/
--- src/script.js
+++ src/script.js
@@ -9,20 +9,20 @@ import '../node_modules/weathericons/sass/weather-icons.min.scss';
 
 import './style.sass';
 
-let celcius, fahrenheit, iconsData;
+let celcius, fahrenheit, iconsData, celciusIsShowed = true;
 
-$(document).ready(function() {
+$(document).ready(() => {
   //вычисляем координаты
     if (navigator.geolocation) {
-	  navigator.geolocation.getCurrentPosition(function(position) {
-	    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&APPID=ca840eee26ce8ed1cb4ecd5aed2c7a5e`;
-	    let weatherIconsUrl = 'https://gist.githubusercontent.com/tbranyen/62d974681dea8ee0caa1/raw/3405bfb2a76b7cbd90fde33d8536f0cd13706955/icons.json';
-	    $.getJSON(weatherUrl)
-            .done(parseWeatherData)
-            .fail(error);
-	    $.getJSON(weatherIconsUrl)
-            .done((data) => {iconsData = data})
-            .fail(error);
+	  navigator.geolocation.getCurrentPosition((position) => {
+	      $.getJSON('https://gist.githubusercontent.com/tbranyen/' +
+              '62d974681dea8ee0caa1/raw/3405bfb2a76b7cbd90fde33d8536f0cd13706955/icons.json')
+              .done((data) => {iconsData = data})
+              .always(() => $.getJSON(`https://api.openweathermap.org/data/2.5/weather?lat=${
+                  position.coords.latitude}&lon=${position.coords.longitude}&APPID=ca840eee26ce8ed1cb4ecd5aed2c7a5e`)
+                  .done(parseWeatherData)
+                  .fail(error)
+              );
 	  });
 	} else {
 		console.error("Geolocation is not supported by this browser.");
@@ -30,45 +30,37 @@ $(document).ready(function() {
 	}
 
 	 // кликаем на цельсий для фаренгейта и наоборот
-	 $('.weather-info__degrees-icon').on('click', function(e){
+	 $('.weather-info__degrees-icon').on('click', function(e) {
          e.preventDefault();
-		 if ($(this).text() === 'C') {
-           $('.weather-info__temperature').html(fahrenheit.toFixed(1)+ '&deg');
-           $(this).html('F');
-		 } else {
-		   $('.weather-info__temperature').html(celcius.toFixed(1)+ '&deg');
-		   $(this).html('C');
-		 }
+         $('.weather-info__temperature').html(`${celciusIsShowed ? fahrenheit : celcius}&deg`);
+         $(this).html(celciusIsShowed ? 'F' : 'C');
+         celciusIsShowed = !celciusIsShowed;
 	});
 });
 
 const parseWeatherData = (data) => {
-        celcius = data.main.temp-273.15;
-        fahrenheit = (celcius*(9/5) + 32);
+        celcius = (data.main.temp - 273.15).toFixed(1);
+        fahrenheit = (celcius * (9/5) + 32).toFixed(1);
 
-        let place = `${data.name}, ${data.sys.country}`;
-        let temp = `${celcius.toFixed(1)}&deg`;
-
-        let code = data.weather[0].id;
-        let icon = iconsData[code].icon;
+        let icon = iconsData ? iconsData[data.weather[0].id].icon : '';
         //отделяем иконки дня от ночи
-        let hour = new Date().getHours();
+        const hour = new Date().getHours();
         icon = (hour < 0 && hour > 6) ? `night-${icon}` : `day-${icon}`;
 
-        showWeatherInfo(place, temp, `wi wi-${icon}`);
+        showWeatherInfo(`${data.name}, ${data.sys.country}`, `wi wi-${icon}`);
 };
 
 //отображение получаемой информации на экране
-const showWeatherInfo = (place, temp, icon) => {
+const showWeatherInfo = (place, icon) => {
     $('.weather-info__place').html(place);
-    $('.weather-info__temperature').html(temp);
+    $('.weather-info__temperature').html(`${celcius}&deg`);
     $('.weather-info__degrees-icon').html('C');
 
     $('.weather-info__icon').addClass(icon);
-}
+};
 
 const error = (error) => {
-    const errorText = `Unfortunately, we couldn't get data about the weather. ${error}`;
+    const errorText = `Unfortunately, we couldn't get data about the weather. ${error.statusText}`;
     alert(errorText);
     console.error(errorText);
 };
